5dac3bfdf4dfbf868712501e2e92fc0a86a315c1
uniqueDev44 <uniquedev44@gmail.com>
2025-11-12 22:21:01 -0800

    get the form and input using the DOM and add an event listener and get the value of the input feilds
    1 create an empty array;
    2 get the form and input using the DOM and add an event listener that submit the form
    3 get the inputs value and save them in a variable
    4 create a new object for the name and expiry date input
    5 push the object to the empty array that was created

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
+let foodArrayList = [];
 
+const addFoodForm = document.querySelector('.add-food-form');
+
+addFoodForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const foodNameInput = document.querySelector('.food-name');
+  const foodName = foodNameInput.value;
+  const expiryDateInput = document.querySelector('input[type="date"]');
+  const expiryDate = expiryDateInput.value;
+
+  const foodItem = {
+    name: foodName,
+    expiry: expiryDate
+  }
+
+  foodArrayList.push(foodItem);
+  console.log(foodArrayList);
+})
 
